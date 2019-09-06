08d35b14f48fbfefb03607a353e396958496800e
Marshal <marshall@fandemand.ca>
2019-09-05 22:23:39 -0400

    added test file.

---
+++ TestJenkins.java
@@ -0,0 +1,14 @@
+
+public class TestJenkins {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String temp="";
+		for(int i=0;i<10;i++) {
+			temp+=i;
+		}
+		System.out.println(temp);
+
+	}
+
+}
